882a70e16046013d82187a2752aee6b240f5cae2
maksim_antipov_1 <anti123456.antipov@yandex.ru>
2023-10-24 22:39:55 +0300

    init

---
+++ src/main/java/ru/araok/AraokApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package ru.araok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AraokApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AraokApiGatewayApplication.class, args);
+    }
+
+}
